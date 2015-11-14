05d49cbdc09502c83fa9b28afeab17ba432b58d7
meriadec <meriadec.pillet@gmail.com>
2015-11-14 16:11:05 +0100

    chore(api): cors

--- api/index.js
+++ api/index.js
@@ -6,6 +6,13 @@ import * as ressources from './ressources'
 const server = express()
 
 server.use(bodyParser.json())
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+})
+
 server.use('/weather', ressources.weather)
 server.use('/bitcoin', ressources.bitcoin)
 server.use('/stack', ressources.stack)
