70ec75925850129fd3c81a097bbdcbfb61f3416f
meriadec <meriadec.pillet@gmail.com>
2015-11-14 03:35:30 +0100

    style(modal): some style

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -9,28 +9,45 @@ class Picker extends Component {
 
   componentWillEnter (done) {
     const drop = findDOMNode(this)
-    const { section } = this.refs
+    const { content } = this.refs
     new TimelineMax()
       .from(drop, 0.1, { opacity: 0 })
-      .from(section, 0.2, { opacity: 0, scale: 0.9 }, '-=0.05')
+      .from(content, 0.2, { opacity: 0, scale: 0.9 }, '-=0.05')
       .addCallback(done)
   }
 
   componentWillLeave (done) {
     const drop = findDOMNode(this)
-    const { section } = this.refs
+    const { content } = this.refs
     new TimelineMax()
-      .to(section, 0.2, { opacity: 0, scale: 0.9 }, '-=0.05')
+      .to(content, 0.2, { opacity: 0, scale: 0.9 }, '-=0.05')
       .to(drop, 0.1, { opacity: 0 })
       .addCallback(done)
   }
 
   render () {
-    console.log(this.props)
     return (
       <div className='Picker'>
-        <section ref='section'>
-          {'Pickers'}
+        <section ref='content'>
+
+          <div className='box-container'>
+            <div className='preview'>
+            </div>
+            <h1>Weather</h1>
+          </div>
+
+          <div className='box-container'>
+            <div className='preview'>
+            </div>
+            <h1>StackOverflow</h1>
+          </div>
+
+          <div className='box-container'>
+            <div className='preview'>
+            </div>
+            <h1>GitHub</h1>
+          </div>
+
         </section>
       </div>
     )
