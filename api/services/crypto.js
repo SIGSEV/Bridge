26d0d6f49895effa9e5155fa4d3b152a3f557a33
Balthazar Gronon <bgronon@gmail.com>
2017-07-22 00:37:19 +0200

    chore(crypto): small shit

--- api/services/crypto.js
+++ api/services/crypto.js
@@ -2,13 +2,22 @@ import q from 'q'
 import got from 'got'
 import cache from 'memory-cache'
 
-const baseUrl = 'https://api.kraken.com/0/public'
+const bittrex = market =>
+  got(`https://bittrex.com/Api/v2.0/pub/market/GetLatestTick?marketName=${market}&tickInterval=oneMin`, { json: true })
+    .then(response => response.body)
+    .then(body => {
+      const data = body.result[0]
+      return {
+        high: data.H,
+        low: data.L,
+        last: data.C,
+        volume: data.V,
+        timestamp: Date.now(),
+      }
+    })
 
-export const getLatest = pair => {
-  const cached = cache.get(`crypto-${pair}`)
-  if (cached) { return q(cached) }
-
-  return got(`${baseUrl}/Ticker?pair=${pair}`, { json: true })
+const kraken = pair =>
+  got(`https://api.kraken.com/0/public/Ticker?pair=${pair}`, { json: true })
     .then(response => response.body)
     .then(body => {
 
@@ -20,12 +29,18 @@ export const getLatest = pair => {
         low: Number(cur.l[0]).toFixed(2),
         last: Number(cur.c[0]).toFixed(2),
         volume: Number(cur.v[0]).toFixed(2),
-        timestamp: new Date().getTime()
+        timestamp: Date.now()
       }
 
     })
-    .then(data => {
-      cache.put('bitcoin', data, 1e3 * 30)
-      return data
-    })
+
+export const getLatest = pair => {
+  const cached = cache.get(`crypto-${pair}`)
+  if (cached) { return q(cached) }
+  const p = pair.includes('-') ? bittrex(pair) : kraken(pair)
+
+  return p.then(data => {
+    cache.put(`crypto-${pair}`, data, 1e3 * 30)
+    return data
+  })
 }
