b5e590d72b22ea311184673e2da0da35856f52ea
Balthazar Gronon <bgronon@gmail.com>
2017-11-26 14:37:28 -0800

    Add json content-type header

--- api/services/deluge.js
+++ api/services/deluge.js
@@ -1,9 +1,13 @@
 import got from 'got'
 
-const r = (host, body, headers = {}) => got.post(`${host}/json`, { body, headers, json: true })
+const r = (host, body, headers = {}) =>
+  got.post(`${host}/json`, {
+    body,
+    headers: { ...headers, 'Content-Type': 'application/json' },
+    json: true,
+  })
 
-const payload = (method, params) =>
-  JSON.stringify({ method, params, id: Date.now() })
+const payload = (method, params) => JSON.stringify({ method, params, id: Date.now() })
 
 const fields = [
   [
@@ -19,20 +23,21 @@ const fields = [
     'total_done',
     'total_uploaded',
   ],
-  []
+  [],
 ]
 
 const query = (host, pass, method, args) =>
   r(host, payload('auth.login', [pass]))
     .then(response => {
-      if (!response.body.result) { throw new Error('Unauthorized.') }
+      if (!response.body.result) {
+        throw new Error('Unauthorized.')
+      }
+
       const cookie = response.headers['set-cookie']
       return r(host, payload(method, args), { cookie })
     })
     .then(response => response.body.result)
 
-export const fetch = (host, pass) =>
-  query(host, pass, 'web.update_ui', fields)
+export const fetch = (host, pass) => query(host, pass, 'web.update_ui', fields)
 
-export const upload = (host, pass, file) =>
-  query(host, pass, 'webapi.add_torrent', [file])
+export const upload = (host, pass, file) => query(host, pass, 'webapi.add_torrent', [file])
