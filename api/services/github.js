6cc222b7ce1c5a56f335a17186414cabd90e402a
Balthazar Gronon <git@balthazar.dev>
2020-02-21 09:47:14 -0800

    Fix github service

--- api/services/github.js
+++ api/services/github.js
@@ -7,36 +7,44 @@ import cheerio from 'cheerio'
 import config from '../../config'
 
 export const getTrending = lang => {
-
   const cached = cache.get(`github-${lang}`)
-  if (cached) { return q(cached) }
+  if (cached) {
+    return q(cached)
+  }
 
-  let url = 'https://github.com/trending'
-  if (lang) { url += `?l=${lang}` }
+  const url = `https://github.com/trending${lang ? `/${lang}` : ''}`
 
   return got(url)
     .then(response => {
-
       const out = []
       const dom = cheerio.load(response.body)
 
-      dom('.repo-list > li')
-        .each((i, it) => {
+      dom('article.Box-row').each((i, it) => {
+        const item = dom(it)
+        const url = item.find('h1 a')[0].attribs.href
+        const name = url.replace(/.*\/(.*)$/, '$1')
 
-          const item = dom(it)
-          const url = item.find('h3 a')[0].attribs.href
-          const name = url.replace(/.*\/(.*)$/, '$1')
+        if (_.includes(config.bannedRepositories, name)) {
+          return
+        }
 
-          if (_.includes(config.bannedRepositories, name)) { return }
+        const desc = item
+          .children('p.text-gray')
+          .text()
+          .trim()
+        const lang = item
+          .find('[itemprop=programmingLanguage]')
+          .text()
+          .trim()
+        const today = item
+          .find('span.float-sm-right')
+          .text()
+          .trim()
+          .split(' ')[0]
 
-          const desc = item.children('.py-1').text().trim()
-          const lang = item.find('[itemprop=programmingLanguage]').text().trim()
-          const today = item.find('span.float-sm-right').text().trim().split(' ')[0]
-
-          const push = { url, name, desc, lang, today }
-          out.push(push)
-
-        })
+        const push = { url, name, desc, lang, today }
+        out.push(push)
+      })
 
       return _.take(out, 15)
     })
